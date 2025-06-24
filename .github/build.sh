ef45c51d2ecd8cb34b1b60a867dbadd83a6a1cbd
karmab <karimboumedhel@gmail.com>
2025-06-24 10:22:09 +0200

    push to pypi

--- .github/build.sh
+++ .github/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -ex
+. venv/bin/activate
 
 mkdir -p src/rhsupportlib/version
 git rev-parse --short HEAD > src/rhsupportlib/version/git
@@ -8,3 +9,8 @@ podman build -t quay.io/karmab/rhsupportcli -f Dockerfile .
 
 podman login -u $QUAY_USERNAME -p $QUAY_PASSWORD quay.io
 podman push quay.io/karmab/rhsupportcli:latest
+
+export VERSION=$(date "+%Y%m%d%H%M")
+sed -i "s/99.0/99.0.$VERSION/" pyproject.toml
+python3 -m build
+twine upload --repository-url https://upload.pypi.org/legacy/ -u __token__ -p $PYPI_TOKEN --skip-existing dist/*
